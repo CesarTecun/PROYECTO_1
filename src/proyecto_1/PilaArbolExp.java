11ced6cb5e54cef2e160873bd4c9a38cc06b6edc
Pispache <pispachea@miumg.edu.gt>
2024-03-15 22:50:51 -0600

    test
    estamos haciendo priuebas

--- src/proyecto_1/PilaArbolExp.java
+++ src/proyecto_1/PilaArbolExp.java
@@ -14,6 +14,7 @@ public class PilaArbolExp {
     public PilaArbolExp() {
         //lo inicializamos como nulo para indicar que la pila
         //esta vacion al principio
+        //
         tope = null;
 
     }
